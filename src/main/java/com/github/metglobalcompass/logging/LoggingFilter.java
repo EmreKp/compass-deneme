2d08fd62f0fe2f7e8a04cfbd3109c3db090f51a8
Emre Kaplan <emre.kaplan@metglobal.com>
2018-08-03 17:54:08 +0300

    Add request handling

--- src/main/java/com/github/metglobalcompass/logging/LoggingFilter.java
+++ src/main/java/com/github/metglobalcompass/logging/LoggingFilter.java
@@ -7,13 +7,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 @Component
@@ -21,11 +22,15 @@ public class LoggingFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		ObjectMapper objectMapper = new ObjectMapper();
 
+		ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
 		ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
 
 		filterChain.doFilter(request, responseWrapper);
+
+		String method = requestWrapper.getMethod();
+		String requestBody = IOUtils.toString(requestWrapper.getInputStream(), "UTF-8");
+		System.out.println("Request method: " + method + " - body: " + requestBody);
 
 		HttpStatus status = HttpStatus.valueOf(responseWrapper.getStatusCode());
 		HttpHeaders headers = new HttpHeaders();
