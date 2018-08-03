a6beb46d5eb4fffa37a883ff69a6f4e8ec583180
Emre Kaplan <emre.kaplan@metglobal.com>
2018-08-03 18:04:48 +0300

    fix request handling

--- src/main/java/com/github/metglobalcompass/logging/LoggingFilter.java
+++ src/main/java/com/github/metglobalcompass/logging/LoggingFilter.java
@@ -6,12 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
@@ -26,18 +25,18 @@ public class LoggingFilter extends OncePerRequestFilter {
 		ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
 		ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
 
-		filterChain.doFilter(request, responseWrapper);
+		filterChain.doFilter(requestWrapper, responseWrapper);
 
 		String method = requestWrapper.getMethod();
+		String url = requestWrapper.getRequestURL().toString();
 		String requestBody = IOUtils.toString(requestWrapper.getInputStream(), "UTF-8");
-		System.out.println("Request method: " + method + " - body: " + requestBody);
+		System.out.println("Request method: " + method + " - params: " + url + " - body: " + requestBody);
 
-		HttpStatus status = HttpStatus.valueOf(responseWrapper.getStatusCode());
+		int status = responseWrapper.getStatusCode();
 		HttpHeaders headers = new HttpHeaders();
 		responseWrapper.getHeaderNames().forEach(
 				header -> headers.add(header, responseWrapper.getHeader(header))
 		);
-
 		String responseBody = IOUtils.toString(responseWrapper.getContentInputStream(), "UTF-8");
 		System.out.println("Status code: " + status + " with response: " + responseBody);
 		responseWrapper.copyBodyToResponse();
